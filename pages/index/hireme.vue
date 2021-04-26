875d8b33bd9d4bbbbfeb9e7125532f99eb49e4c9
sopegue <yayasopeguesoro@gmail.com>
2021-04-27 00:34:48 +0100

    wip

--- pages/index/hireme.vue
+++ pages/index/hireme.vue
@@ -26,6 +26,45 @@
           ></path>
         </svg>
         Get my resume
+        <a
+          href="/resume/cv.pdf"
+          target="_blank"
+          class="ml-5 sombre-text"
+          title="Open the resume in a new tab"
+        >
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            ></path></svg
+        ></a>
+        <a
+          href="/resume/cv.pdf"
+          class="ml-3 sombre-text"
+          download
+          title="Download the resume"
+          ><svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+            ></path></svg
+        ></a>
       </a>
     </div>
   </div>
