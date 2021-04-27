6916458f998be37f0588249c0a9acdd15485fab5
sopegue <yayasopeguesoro@gmail.com>
2021-04-27 01:42:52 +0100

    wip

--- aboutme/about.js
+++ aboutme/about.js
@@ -1,6 +1,6 @@
 const about = [
-  "Hi, I'm Sopegue Yaya SORO aka Yaya Sopegue SORO or SORO Yaya Sopegue. The last one is the mostly form used in Ivory Coast.",
-  "I'm from the noth region of Ivory coast called PORO which is in my parents languages the 'Senoufo'. I grew up in the city of Korhogo  mostly with my grand-mother.",
+  "Hi, I'm Sopegue Yaya SORO aka Yaya Sopegue SORO or SORO Yaya Sopegue. The last one is the mostly form used in Ivory Coast but you can call me Sopegue pronounced « Sopégué » (in my native language).",
+  "I'm from the north region of Ivory coast (Côte d'Ivoire in French) called PORO which is in my parents languages the 'Senoufo' and also my native language. I grew up in the city of Korhogo  mostly with my grand-mother.",
   "I'm a student with a bachelor degree in computer science and currently studying a master in systems informations engineering. I do code, mostly website. I like learning and practicing by doing projects. If set up a project from the front end to the backend until deploying the project and having a project that works is called full stack, then I can call myself a full stack developper. I work mostly with javascript, html and css as programming languages, vue js & nuxt js as front-end framework, laravel as backend-end framework, Postgresql & Mysql as database, Microsoft azure for database deployment and management, heroku for the back-end deployment and Netlify for the front-end deployment. I use git and github for project version & continuous integrations management.",
 ]
 export default about
