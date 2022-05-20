84050818f1e2e0e4b48c1ec8097a66cf5b93201a
Xunita <42295537+xunita@users.noreply.github.com>
2022-05-21 01:10:33 +0200

    Update Afterheader.vue

--- components/Afterheader.vue
+++ components/Afterheader.vue
@@ -8,7 +8,7 @@
         class="flex md:flex-row flex-col items-center md:px-30 px-10 lg:space-x-20 sm:space-x-10 md:space-y-0 space-y-10"
       >
         <figure class="image w-70 h-70 min-h-70 min-w-70 rounded-full">
-          <img class="rounded-full w-full border-4 h-full" src="/pic/3.png" />
+          <img class="rounded-full w-full border-4 h-full" src="/pic/mypic.jpg" />
         </figure>
         <div class="h-fit">
           <p>
